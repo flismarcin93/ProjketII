f144eed49804c55e220b5d60fe136cc49331c33f
flismarcin <flis.marcin93@gmail.com>
2016-10-20 22:41:33 +0200

    Aplikacja jeden kontroller jedna akcja

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on October 20, 2016, 10:02 pm.
---
+++ src/AppBundle/Controller/FrontController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class FrontController extends Controller
+{
+    /**
+     * @Route("/index")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Front:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
